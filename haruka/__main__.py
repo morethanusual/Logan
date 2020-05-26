b5eb4ce46635285c55d81185a05fd656758d6948
deepu0077 <51230074+deepu0077@users.noreply.github.com>
2020-05-26 16:28:44 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -135,12 +135,12 @@ def send_start(bot, update):
         pass
 
     #chat = update.effective_chat  # type: Optional[Chat] and unused variable
-    text = "Hey there! My name is Haruka Aya - I'm here to help you manage your groups!\n\
-Click Help button to find out more about how to use me to my full potential.\n\n"
+    text = " Hello {}, my name is {}! - I'm here to help you manage your groups!\n\
+Click Help or /help.. button to find out more about how to use me to my full potential.\n\n"
 
-    text += "Join [Haruka Aya Group](https://t.me/HarukaAyaGroup) ( @HarukaAyaGroup ) if you need any support or help\n\n\
-Follow [Haruka Aya](https://t.me/HarukaAya) ( @HarukaAya ) if you want to keep up with the news, updates and bot downtime!\n\n\
-Made with love by @RealAkito\n\nWant to add me to your group? [Click here!](t.me/HarukaAyaBot?startgroup=true)"
+    text += " The Bot Has Made With LOVE By [Ben 10!!](https://t.me/Baby_xD)"
+
+
 
     keyboard = [[InlineKeyboardButton(text="📢 Support Group", url="https://t.me/HarukaAyaGroup")]]
     keyboard += [[InlineKeyboardButton(text="🛠 Control panel", callback_data="cntrl_panel_M")]]
